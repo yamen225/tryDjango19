57dc5e4b33b4ed68e8c4417136188160f2e830f4
yamen <yamen225@gmail.com>
2018-08-10 21:16:38 +0200

    adding static routes - css

--- src/posts/views.py
+++ src/posts/views.py
@@ -32,7 +32,7 @@ def post_list(request):
 		"object_list":queryset,
 		"title": "list",
 	}
-	return render(request, "post_list.html", context)
+	return render(request, "post-list.html", context)
 
 def post_update(request, id= None):
 	instance = get_object_or_404(Post,id=id)
